a55c04f192e69417722f4b6c35bb1fc8b69c7796
habibullo27 <khsanin2727@gmail.com>
2024-10-25 23:21:23 +0500

    fhhh

---
+++ buttons.py
@@ -0,0 +1,13 @@
+from telebot import types
+
+def phone_button():
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button = types.KeyboardButton('Поделиться номер', request_contact=True)
+    kb.add(button)
+    return kb
+
+def location_button():
+    kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button = types.KeyboardButton('Поделиться локацией', request_location=True)
+    kb.add(button)
+    return kb
